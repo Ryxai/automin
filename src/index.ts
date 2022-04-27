8d774571c970f0d6c0683fb4dd102a09ae446cdc
Ryxai <ryxai@ryxai.com>
2022-04-27 12:18:19 -0400

    Updated logging and start commands

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import axios from 'axios';
+import process from 'process';
 import express from 'express';
 import bodyParser from 'body-parser';
 
-const port = '9540';
+const port = process.argv.length > 2 ? process.argv[2] : '9540';
 const app = express();
 app.use(bodyParser.json());
+
 
 //CORS handling for Marvin
 app.options('/*',(req, res) => {
@@ -18,15 +20,15 @@ app.options('/*',(req, res) => {
 //Linking Rescuetime to Marvin for timer start/stop etc
 app.post('/rescue_time_start', (req, res) => {
   const duration = Math.ceil(((req.body.isWork ? req.body.workDuration - req.body.elapsed : req.body.breakDuration - req.body.elapsed)/60000)/5)*5;
-  console.log(req.body);
-  console.log(`Passing rescuetime a session duration of ${duration}`);
+//  console.log(req.body);
+  console.log(`Passing rescuetime a start session prompt with a duration of ${duration} minutes`);
   axios.post(`https://www.rescuetime.com/anapi/start_focustime?key=${req.get('X-Api-Key')}&duration=${duration}`)
     .then((response) => {/*console.log(response.data);*/res.sendStatus(200);});
 });
 
 app.post('/rescue_time_end', (req, res) => {
-  console.log(req.body);
-  console.log(`Passing rescuetime an end session`);
+ // console.log(req.body);
+  console.log(`Passing rescuetime an end session prompt`);
   axios.post(`https://www.rescuetime.com/anapi/end_focustime?key=${req.get('X-Api-Key')}`)
     .then((response) => {/*console.log(response.data);*/res.sendStatus(200);});
 });
