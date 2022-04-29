d9de5dfd431451b451ba1957c7ed97c07ee0fb98
Ryxai <ryxai@ryxai.com>
2022-04-29 08:15:37 -0400

    Fixed duration structure to deal with standard timers as well

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ app.post('/rescue_time_start', (req, res) => {
   res.set("Access-Control-Allow-Headers", "Content-Type, contenttype, X-Api-Key, Access-Control-Allow-Methods,Access-Control-Allow-Origin");
   res.set("Access-Control-Allow-Methods", "OPTIONS, POST");
   res.set("Access-Control-Allow-Origin","*");
-  const duration = Math.ceil(((req.body.isWork ? req.body.workDuration - req.body.elapsed : req.body.breakDuration - req.body.elapsed)/60000)/5)*5;
+  const duration = Math.ceil(((req.body.isWork ? req.body.workDuration ?? req.body.duration - req.body.elapsed : req.body.breakDuration - req.body.elapsed)/60000)/5)*5;
   //console.log(req.body);
   console.log(`Passing rescuetime a start session prompt with a duration of ${duration} minutes`);
   axios.post(`https://www.rescuetime.com/anapi/start_focustime?key=${req.get('X-Api-Key')}&duration=${duration}`)
